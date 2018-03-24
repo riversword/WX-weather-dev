901f388f354ffaf183d8fe4f03f444f1e172eca3
riversword <2694473261@qq.com>
2018-03-24 23:45:38 +0800

    微信小程序-天气

---
+++ pages/weather/weather.js
@@ -0,0 +1,12 @@
+Page({
+
+  onLoad: function(){
+    var weatherData ;
+    wx.request({
+      url: 'https://free-api.heweather.com/v5/weather?city=113.12,23.02&key=45f089df10e64f3591d88f0a2c6c7dc5',
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  }
+});
