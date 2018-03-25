7eb1d509ccbd807725096bab1c49a8e0943f5cd5
riversword <2694473261@qq.com>
2018-03-25 23:33:18 +0800

    做完下雪 特效

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -2,11 +2,11 @@ Page({
 
   onLoad: function(){
     var weatherData ;
-    wx.request({
-      url: 'https://free-api.heweather.com/v5/weather?city=113.12,23.02&key=45f089df10e64f3591d88f0a2c6c7dc5',
-      success: function (res) {
-        console.log(res.data)
-      }
-    })
+    // wx.request({
+    //   url: 'https://free-api.heweather.com/v5/weather?city=113.12,23.02&key=45f089df10e64f3591d88f0a2c6c7dc5',
+    //   success: function (res) {
+    //     console.log(res.data)
+    //   }
+    // })
   }
 });
